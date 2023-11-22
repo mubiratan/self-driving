1dafd579ec953092a708deae0dac9b4440742ba8
mubiratan <mubiratan@gmail.com>
2023-11-22 17:33:42 -0300

    Retirada do build e devel

--- bumperbot_ws/build/bumperbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcio/estudos/udemy/self-driving/bumperbot_ws/src/bumperbot_description
-# Build directory: /home/marcio/estudos/udemy/self-driving/bumperbot_ws/build/bumperbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- bumperbot_ws/build/bumperbot_examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcio/estudos/udemy/self-driving/bumperbot_ws/src/bumperbot_examples
-# Build directory: /home/marcio/estudos/udemy/self-driving/bumperbot_ws/build/bumperbot_examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
