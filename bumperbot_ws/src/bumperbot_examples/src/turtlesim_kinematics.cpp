81366bb343414a622103c1385502f03a3cad3b39
mubiratan <mubiratan@gmail.com>
2023-11-24 22:05:50 -0300

    Kinematics Final

--- bumperbot_ws/src/bumperbot_examples/src/turtlesim_kinematics.cpp
+++ bumperbot_ws/src/bumperbot_examples/src/turtlesim_kinematics.cpp
@@ -18,8 +18,16 @@ void TurtlesimKinematics::turtle2PoseCallBack(const turtlesim::Pose& pose)
     float Tx = last_turtle2_pose.x - last_turtle1_pose.x;
     float Ty = last_turtle2_pose.y - last_turtle1_pose.y;
 
-    ROS_INFO_STREAM("------------------------------\n" <<
-                    "Translation Vector turtle1 > turtle2 \n" <<
+    float theta_rad = last_turtle2_pose.theta - last_turtle1_pose.theta;   
+    float theta_deg = 180 * theta_rad / 3.14; 
+
+    ROS_INFO_STREAM("------------------------------------\n" <<
+                    "Translation Vector turtle1 -> turtle2 \n" <<
                     "Tx: " << Tx << "\n" <<
-                    "Ty: " << Ty << "\n");
+                    "Ty: " << Ty << "\n" <<
+                    "Rotation Matrix turtle1 -> turtle2 \n" << 
+                    "theta (rad): " << theta_rad << "\n" <<
+                    "theta (deg): " << theta_deg << "\n" <<
+                    "|R11   R12|:  |" << std::cos(theta_rad) << "\t" << -std::sin(theta_rad) << "|\n" <<
+                    "|R21   R22|   |" << std::sin(theta_rad) << "\t\t" << std::cos(theta_rad) << "|\n");
 }
